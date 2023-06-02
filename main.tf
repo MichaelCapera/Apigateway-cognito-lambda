e1418fd141304b2530c4211bbe36849585fd8f1e
MAC <michaelcapera@gmail.com>
2023-06-02 06:58:56 -0600

    New resoruce path

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource "aws_api_gateway_rest_api" "rest_api" {
   name        = "royal-api"
   description = "Royal API Gateway"
 }
+
+resource "aws_api_gateway_resource" "check_in_resource" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  path_part   = "check-in"
+}
